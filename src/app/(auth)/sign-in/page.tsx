2a6fa9da7fff09b670e0fa19ae21634c477f31a1
Yuvi dew <yuvraj@truelink.ai>
2025-07-10 06:22:34 +0530

    feat : creating a sign in page and import the sign in form

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SignInForm } from '../_components/SignInForm'
+
+const  SignIn = () => {
+    return (
+        <>
+            {/* start to adding sign in form */}
+            <SignInForm/> 
+            {/* end to adding sign in form */}
+        </>
+    )
+}
+
+export default SignIn
