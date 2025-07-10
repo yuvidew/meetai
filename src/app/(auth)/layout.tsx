a641a0c6d363ce5bae20bac5823cbbabb66051bb
Yuvi dew <yuvraj@truelink.ai>
2025-07-10 06:21:12 +0530

    feat : creating a auth layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <main className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+            <div className="w-full max-w-sm">
+                {children}
+            </div>
+        </main>
+    );
+}
