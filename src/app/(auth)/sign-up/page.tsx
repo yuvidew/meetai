876a30fca6794aea808de2efac8022515a66b363
Yuvi dew <yuvraj@truelink.ai>
2025-07-10 06:23:09 +0530

    feat: creating a sign up page and importing a singup form

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SignUpForm } from '../_components/SignUpForm'
+
+
+const  SignUp = () => {
+    return (
+        <>
+            {/* start to adding a  sign in form  */}
+                <SignUpForm/>
+            {/* end to adding a  sign in form  */}
+        </>
+    )
+}
+
+export default SignUp
